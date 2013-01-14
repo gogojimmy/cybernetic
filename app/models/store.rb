fb565c300ac2aa18dfcc3d846d79ae28384b14cc
Jimmy Kuo <jimmy@gogojimmy.net>
2013-01-14 13:16:33 +0800

    Adjust store view and download i18n

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,6 @@
 class Store < ActiveRecord::Base
   attr_accessible :address, :email, :name, :store_type, :tel, :url
 
-  STORE_TYPES = %w( 獨立店 3C賣場 生活賣場 網路賣場 )
+  STORE_TYPES = %w( 獨立店 3C賣場 生活賣場 網路賣場 其他 )
   validates_presence_of :name
 end
