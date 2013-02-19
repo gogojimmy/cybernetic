2a956375585601ea4af0396ad91230e1a08124f1
Jimmy Kuo <jimmy@gogojimmy.net>
2013-02-19 23:41:23 +0800

    Change to user cybernetic admin && fix admin form

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :rails_env, "production"
-set :user, "cybernetic"
-set :domain, "cybernetic.gogojimmy.net"
+set :user, "deployer"
+set :domain, "66.175.219.39"
 set :branch, "master"
 
 server "#{domain}", :web, :app, :db, :primary => true
