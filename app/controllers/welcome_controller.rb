dace47bd09444dcc80597473dee7f7b6ab812cc7
Jimmy Kuo <jimmy@gogojimmy.net>
2013-03-05 11:21:17 +0800

    Change slider order

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 class WelcomeController < ApplicationController
   layout 'welcome'
   def index
-    @sliders = Slider.where(block: "首頁橫幅廣告").include_locale(params[:locale])
+    @sliders = Slider.where(block: "首頁橫幅廣告").order("position").include_locale(params[:locale])
     @news = Post.news.include_locale(params[:locale]).last(5)
   end
 end
