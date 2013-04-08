852612c874294d33eee197368220e59122367793
Jimmy Kuo <jimmy@gogojimmy.net>
2013-04-09 01:48:39 +0800

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Cybernetic::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'http://gogojimmy.net' }
+  config.action_mailer.default_url_options = { :host => 'www.cybernetic.com.tw' }
 
   config.assets.precompile += %w( admin.js admin.css ie.css welcome.css )
 
