2022232ef78295c68c3d136dc33383dbbbf36396
Jimmy Kuo <jimmy@gogojimmy.net>
2013-04-11 12:13:31 +0800

    remove gogojimmy email

--- app/mailers/contact_notice_mailer.rb
+++ app/mailers/contact_notice_mailer.rb
@@ -4,6 +4,5 @@ class ContactNoticeMailer < ActionMailer::Base
   default from: 'service@cybernetic.com.tw'
   def send_contact_notice_mail
     mail to: 'service@cybernetic.com.tw', subject: '意見提醒'
-    mail to: 'jimmy@gogojimmy.net', subject: 'Cybetnetic notification'
   end
 end
